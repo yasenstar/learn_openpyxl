db39af4f58d79e067cdbd6304507102dd6ed24ba
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2025-12-30 15:26:58 +0800

    update

--- Mastering_openpyxl.mm
+++ Mastering_openpyxl.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.12.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Mastering openpyxl" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1766817575719"><hook NAME="MapStyle" background="#f5f5dcff">
+<node TEXT="Mastering openpyxl" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1766817575719"><hook NAME="MapStyle" background="#f5f5dcff" zoom="0.82644624">
     <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/light_sepia_template.mm" fit_to_viewport="false" show_icons="BESIDE_NODES"/>
     <tags category_separator="::"/>
 
